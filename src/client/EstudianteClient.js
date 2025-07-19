af19b1b31f9f8329cca093abbefe70b244f4080d
fcfiallos <fcfiallos@uce.edu.ec>
2025-07-18 19:26:49 -0500

    taller 39: token vue

--- src/client/EstudianteClient.js
+++ src/client/EstudianteClient.js
@@ -5,7 +5,14 @@ const URL_API = "http://localhost:8081/api/matricula/v1/estudiantes";
 // public void guardar(@RequestBody EstudianteTo estudianteTo)
 
 const guardar = async (body) => {
-    const data = axios.post(`${URL_API}`, body).then(resp => resp.data);
+    let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdWNlLmVkdS5lYyIsInVwbiI6ImZjZmlhbGxvc0B1Y2UuZWR1LmVjIiwiZ3JvdXBzIjpbImFkbWluIl0sImlhdCI6MTc1Mjg4NDY0MywiZXhwIjoxNzUyODg0NjczLCJqdGkiOiJjMDRkZGMxOC1lNjA5LTQxOTYtYjA2YS1kZDM5NTUwY2QyZDgifQ.ZX0dzEXGdEr6g3XNGZNP002DPWsEixeZIUoLIy56GpDYHfSGW-Ecd1hj913RJfx-ELkuQwVjHL9B9UbmDlZNgeoBx_Xl1MntpUjkTLUW1KDSbCUcyty9HW_2krKIfpFnzTV2whiAo17LHomrjlwFNSvBs7wlD_gA3cCZ6JZNEenfTZGCmaY_u-uD0f0aMOirdZxbHch_OWzLRptS1nxtXV-lR1_-cKRSCHWrTYyagezXVYrWTwPgY1I-mlU4kIg7QUIX2zij707pVWyelX4GizQ3BR1ezOgrsxbK-VSBlrRZ4u8GG_LscDjJOg-UFD0eZB8wX_liUdK8HxbuuPI-GA'
+    const data = axios.post(`${URL_API}`, body,
+        {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }
+    ).then(resp => resp.data);
     console.log(data);
 }
 
